e9c1b7dd641b37b7b571f68a0d08d5e5919715bd
muskij <musamustaphamm5503116@gmail.com>
2026-09-13 21:38:46 +0100

    favicon

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -5,13 +5,7 @@ export default function SiteHeader({ user, subscribed }) {
   return (
     <header className="site-header">
       <Link className="logo" href="/" aria-label="MedPhysio Tutorials home">
-        <span className="logo-mark">
-          M<span>+</span>
-        </span>
-        <span className="logo-copy">
-          <strong>MedPhysio</strong>
-          <small>Tutorials</small>
-        </span>
+        <img src="/assets/img/logo.png" alt="MedPhysio Tutorials" className="logo-img" />
       </Link>
       <nav aria-label="Main navigation">
         <Link href="/#courses">Courses</Link>
