18cb3b152b364b6e5b6b455f8378d38bc82fd8f1
muskij <musamustaphamm5503116@gmail.com>
2026-09-13 19:07:59 +0100

    yep

--- app/api/auth/me/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getSessionUser } from "../../../../lib/auth";
-
-export async function GET() {
-  const user = await getSessionUser();
-  return NextResponse.json({ user });
-}
